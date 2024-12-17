53df44dd46fee1c8b51fb5d75552433240c2aa5c
uriy <78085017+monstebit@users.noreply.github.com>
2024-12-17 20:55:48 +0300

    add DataPage

--- RevitPlague/Views/ManualTests/DataPage.xaml.cs
+++ RevitPlague/Views/ManualTests/DataPage.xaml.cs
@@ -1,11 +1,20 @@
-using System.Windows.Controls;
+using RevitPlague.Services.Contracts;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
 
 namespace RevitPlague.Views.ManualTests;
 
-public partial class DataPage : Page
+public partial class DataPage : FluentWindow, IWindow
 {
     public DataPage()
     {
         InitializeComponent();
+        ApplicationThemeManager.Apply(this);
     }
+    
+    public void Close() => base.Close();
+
+    public void Show() => base.Show();
+
+    public bool? ShowDialog() => base.ShowDialog();
 }
