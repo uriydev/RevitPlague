be5039dcb865b6863352dedc01955a7e71d4fa82
uriy <78085017+monstebit@users.noreply.github.com>
2024-12-27 17:02:32 +0300

    delete ObservableCollection from MainWindow VM

--- RevitPlague/ViewModels/MainWindowViewModel.cs
+++ RevitPlague/ViewModels/MainWindowViewModel.cs
@@ -7,43 +7,7 @@ namespace RevitPlague.ViewModels;
 
 public partial class MainWindowViewModel : ObservableObject
 {
-    [ObservableProperty]
-    private ObservableCollection<NavigationViewItem> _navigationItems;
-
-    [ObservableProperty]
-    private ObservableCollection<object> _navigationFooter;
-
     public MainWindowViewModel()
     {
-        InitializeViewModel();
-    }
-
-    private void InitializeViewModel()
-    {
-        NavigationItems = new ObservableCollection<NavigationViewItem>
-        {
-            new NavigationViewItem
-            {
-                Content = "Home|DashboardPage",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.Home24 },
-                TargetPageType = typeof(DashboardPage)
-            },
-            new NavigationViewItem
-            {
-                Content = "Data",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.DataHistogram24 },
-                TargetPageType = typeof(DataPage)
-            }
-        };
-
-        NavigationFooter = new ObservableCollection<object>
-        {
-            new NavigationViewItem
-            {
-                Content = "Settings",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 },
-                TargetPageType = typeof(SettingsPage)
-            }
-        };
     }
 }
