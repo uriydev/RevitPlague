14e9e99076e672c720bafd3ee843e68c1da37862
uriy <78085017+monstebit@users.noreply.github.com>
2025-01-11 12:44:29 +0300

    delete comments from PlagueViewCommand

--- RevitPlague/Commands/PlagueViewCommand.cs
+++ RevitPlague/Commands/PlagueViewCommand.cs
@@ -1,12 +1,8 @@
+using System.Windows;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Microsoft.Extensions.DependencyInjection;
-using RevitPlague.Contracts;
-using RevitPlague.Models;
-using RevitPlague.ViewModels;
 using RevitPlague.Views;
-using SimpleInjector;
 
 namespace RevitPlague.Commands;
 
@@ -19,62 +15,9 @@ public class PlagueViewCommand : IExternalCommand
         ElementSet elements)
     {
         var view = Host.GetService<RevitPlagueView>();
-        view.Show();
+        view.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        view.Show(commandData.Application.MainWindowHandle);
 
         return Result.Succeeded;
     }
 }
-// public class PlagueViewCommand : ExternalCommand
-// {
-//     public override void Execute()
-//     {
-//         var view = Host.GetService<RevitPlagueView>();
-//         view.Show();
-//     }
-// }
-// public class PlagueViewCommand : IExternalCommand
-// {
-//     public Result Execute(
-//         ExternalCommandData commandData,
-//         ref string message,
-//         ElementSet elements)
-//     {
-//         var uiDocument = commandData
-//             .Application
-//             .ActiveUIDocument;
-//
-//         var app = commandData
-//             .Application
-//             .Application;
-//
-//         var container = ConfigureUI();
-//
-//         container.RegisterInstance(app);
-//         container.RegisterInstance(uiDocument);
-//
-//         container.Register<IGetEntities, GetElementsService>();
-//         container.Register<IPickEntities, PickElementsService>();
-//         container.Register<IZoomEntity, ZoomElementService>();
-//         container.Register<IWatchDocument, WatchDocumentService>();
-//         container.Register<IDeleteEnitity, DeleteElementService>();
-//         container.Register<ElementToDTOConverter>();
-//
-//         var window = container
-//             .GetInstance<MainWindow>();
-//
-//         window.Show();
-//
-//         return Result.Succeeded;
-//     }
-//
-//     public static Container ConfigureUI()
-//     {
-//         var container = new Container();
-//         container.Register<GetEntitiesCommand>();
-//         container.Register<PickEntitiesCommand>();
-//         container.Register<ZoomEntityCommand>();
-//         container.Register<MainViewModel>();
-//         container.Register<MainWindow>();
-//         return container;
-//     }
-// }
