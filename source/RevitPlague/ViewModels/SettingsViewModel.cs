6663549edc51caccec45c25a596f730d48c60acc
uriy <78085017+monstebit@users.noreply.github.com>
2025-01-19 14:13:22 +0300

    clear test code from settings vm

--- source/RevitPlague/ViewModels/SettingsViewModel.cs
+++ source/RevitPlague/ViewModels/SettingsViewModel.cs
@@ -1,46 +1,10 @@
-using Autodesk.Revit.UI.Selection;
-using CommunityToolkit.Mvvm.Input;
-using Autodesk.Revit.UI;
 using CommunityToolkit.Mvvm.ComponentModel;
-using RevitPlague.Core.Services;
 
 namespace RevitPlague.ViewModels;
 
 public partial class SettingsViewModel : ObservableObject
 {
-    public SettingsViewModel(RevitApiTaskExecutor revitApiTaskHandler)
+    public SettingsViewModel()
     {
-        RevitApiTaskHandler = revitApiTaskHandler;
-    }
-
-    public RevitApiTaskExecutor RevitApiTaskHandler { get; }
-
-    [RelayCommand]
-    private void PickAndZoomInstance()
-    {
-        RevitApiTaskHandler.ExecuteAsync(application =>
-        {
-            var activeUiDocument = application.ActiveUIDocument;
-            if (activeUiDocument == null)
-            {
-                TaskDialog.Show("Ошибка", "Нет активного документа Revit.");
-                return;
-            }
-            
-            var selection = activeUiDocument.Selection;
-            
-            try
-            {
-                var pickedObject = selection.PickObject(ObjectType.Element);
-                var elementId = pickedObject.ElementId;
-                var document = activeUiDocument.Document;
-                var element = document.GetElement(elementId);
-                activeUiDocument.ShowElements(element);
-            }
-            catch (Exception ex)
-            {
-                TaskDialog.Show("Ошибка", ex.Message);
-            }
-        });
     }
 }
