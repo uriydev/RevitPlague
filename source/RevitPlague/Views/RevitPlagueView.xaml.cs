5dc2ae6224fc0614491537774e5cbfb10fa8f638
uriydev <78085017+uriydev@users.noreply.github.com>
2025-01-19 12:15:18 +0300

    add view data context, delete Relay Commands for RevitPlagueView

--- source/RevitPlague/Views/RevitPlagueView.xaml.cs
+++ source/RevitPlague/Views/RevitPlagueView.xaml.cs
@@ -1,7 +1,4 @@
-using System.Windows.Input;
 using System.Windows.Interop;
-using CommunityToolkit.Mvvm.Input;
-using RevitPlague.Core.Services;
 using RevitPlague.Views.Pages;
 using Wpf.Ui;
 
@@ -9,40 +6,11 @@ namespace RevitPlague.Views;
 
 public partial class RevitPlagueView
 {
-    public ICommand NavigateToHomeCommand { get; }
-    public ICommand NavigateToSettingsCommand { get; }
-
-    public RevitPlagueView(
-        INavigationService navigationService, 
-        RevitApiTaskExecutor revitApiTaskHandler)
+    public RevitPlagueView(INavigationService navigationService)
     {
         InitializeComponent();
-        
-        RevitApiTaskHandler = revitApiTaskHandler;
         navigationService.SetNavigationControl(RootNavigation);
-
-        NavigateToHomeCommand = new RelayCommand(NavigateToHome);
-        NavigateToSettingsCommand = new RelayCommand(NavigateToSettings);
-        
         Loaded += (_, _) => RootNavigation.Navigate(typeof(HomePage));
-    }
-
-    public RevitApiTaskExecutor RevitApiTaskHandler { get; }
-
-    private void NavigateToHome()
-    {
-        RevitApiTaskHandler.ExecuteAsync(application =>
-        {
-            RootNavigation.Navigate(typeof(HomePage));
-        });
-    }
-
-    private void NavigateToSettings()
-    {
-        RevitApiTaskHandler.ExecuteAsync(application =>
-        {
-            RootNavigation.Navigate(typeof(SettingsPage));
-        });
     }
     
     // Перегруженный метод Show
